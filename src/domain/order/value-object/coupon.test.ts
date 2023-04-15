4f2a2b827026b6ef9ea3831ff7a1fb5b4f284bc9
Vyctor <dev.vyctor@gmail.com>
2023-04-14 23:58:50 -0300

    feat: implementando value-object Coupon

---
+++ src/domain/order/value-object/coupon.test.ts
@@ -0,0 +1,86 @@
+import { Coupon } from './coupon';
+
+describe('Coupon Entity Unit Test', () => {
+  it('should create a new coupon', () => {
+    const props = {
+      code: 'VALE20',
+      percentage: 20,
+      expirationDate: new Date('2023-12-31'),
+    };
+    const coupon = new Coupon(props);
+    expect(coupon.code).toBe(props.code);
+    expect(coupon.percentage).toBe(props.percentage);
+    expect(coupon.expirationDate).toBe(props.expirationDate);
+  });
+
+  it('should be able to verify if a coupon is expired or not', () => {
+    const props = {
+      code: 'VALE20',
+      percentage: 20,
+      expirationDate: new Date('2023-12-31'),
+    };
+    const coupon = new Coupon(props);
+    expect(coupon.isExpired()).toBe(false);
+  });
+
+  it('should not create a coupon with percentage less than zero', () => {
+    const props = {
+      code: 'VALE20',
+      percentage: -20,
+      expirationDate: new Date('2023-12-31'),
+    };
+    expect(() => new Coupon(props)).toThrowError('Percentage must be between 0 and 100');
+  });
+  it('should not create a coupon with percentage greater than 100', () => {
+    const props = {
+      code: 'VALE20',
+      percentage: 120,
+      expirationDate: new Date('2023-12-31'),
+    };
+    expect(() => new Coupon(props)).toThrowError('Percentage must be between 0 and 100');
+  });
+
+  it('should not create a coupon with expiration date less than today', () => {
+    const props = {
+      code: 'VALE20',
+      percentage: 20,
+      expirationDate: new Date('2020-12-31'),
+    };
+    expect(() => new Coupon(props)).toThrowError('Expiration date must be greater than today');
+  });
+
+  it('should not create a coupon with code less than 3 characters', () => {
+    const props = {
+      code: 'VA',
+      percentage: 20,
+      expirationDate: new Date('2023-12-31'),
+    };
+    expect(() => new Coupon(props)).toThrowError('Code must have between 3 and 20 characters');
+  });
+
+  it('should not create a coupon with code greater than 20 characters', () => {
+    const props = {
+      code: 'VALE20VALE20VALE20000',
+      percentage: 20,
+      expirationDate: new Date('2023-12-31'),
+    };
+    expect(() => new Coupon(props)).toThrowError('Code must have between 3 and 20 characters');
+  });
+
+  it('should not create a coupon with code with special characters', () => {
+    const props = {
+      code: 'VALE20#',
+      percentage: 20,
+      expirationDate: new Date('2023-12-31'),
+    };
+    expect(() => new Coupon(props)).toThrowError('Code must have only letters and numbers');
+  });
+  it('should not create a coupon with code with white spaces', () => {
+    const props = {
+      code: 'VALE 20',
+      percentage: 20,
+      expirationDate: new Date('2023-12-31'),
+    };
+    expect(() => new Coupon(props)).toThrowError('Code must have only letters and numbers');
+  });
+});
---
+++ src/domain/order/value-object/coupon.ts
@@ -0,0 +1,57 @@
+import { BaseEntity } from '../../shared/entity/base-entity';
+import { type Id } from '../../shared/value-object/id';
+
+interface CouponProps {
+  id?: Id;
+  code: string;
+  percentage: number;
+  expirationDate: Date;
+}
+
+export class Coupon extends BaseEntity {
+  private readonly _code: string;
+  private readonly _percentage: number;
+  private readonly _expirationDate: Date;
+
+  constructor(props: CouponProps) {
+    super(props.id);
+    this._code = props.code;
+    this._percentage = props.percentage;
+    this._expirationDate = props.expirationDate;
+    this.validate();
+  }
+
+  get code(): string {
+    return this._code;
+  }
+
+  get percentage(): number {
+    return this._percentage;
+  }
+
+  get expirationDate(): Date {
+    return this._expirationDate;
+  }
+
+  public isExpired(): boolean {
+    return this._expirationDate.getTime() < new Date().getTime();
+  }
+
+  private validate(): void {
+    if (this._percentage < 0 || this._percentage > 100) {
+      throw new Error('Percentage must be between 0 and 100');
+    }
+
+    if (this._expirationDate.getTime() < new Date().getTime()) {
+      throw new Error('Expiration date must be greater than today');
+    }
+
+    if (this._code.length < 3 || this._code.length > 20) {
+      throw new Error('Code must have between 3 and 20 characters');
+    }
+
+    if (this._code.match(/[^a-zA-Z0-9]/) != null) {
+      throw new Error('Code must have only letters and numbers');
+    }
+  }
+}
