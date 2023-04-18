a56497d33b2fc6c157e7fba20c9d883fde64b0af
Vyctor <dev.vyctor@gmail.com>
2023-04-17 22:17:50 -0300

    test: adicionando mais casos de uso de teste

--- src/domain/shared/value-object/cpf.test.ts
+++ src/domain/shared/value-object/cpf.test.ts
@@ -1,10 +1,13 @@
 import { Cpf } from './cpf';
 
 describe('Cpf Value Object Unit Test', () => {
-  it('should create a new cpf with the provided value', () => {
-    const cpf = new Cpf('700.966.211-84');
-    expect(cpf.value).toBe('700.966.211-84');
-  });
+  test.each(['407.302.170-27', '684.053.160-00', '746.971.314-01', '25169484097', '06381925019'])(
+    'should create a new cpf with the provided value',
+    (value) => {
+      const cpf = new Cpf(value);
+      expect(cpf.value).toBe(value);
+    },
+  );
 
   it('should throw an error if cpf if invalid', () => {
     expect(() => {
